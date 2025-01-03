c43e7ce9f53cca6a3da11da2ca74cba2b5294daf
Wiktor Rembowski <wiktorrem@lts.mat.umk.pl>
2025-01-03 15:48:47 +0100

    Zmieniono program.cpp by wypisywal silnie podanej liczby

--- program.cpp
+++ program.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+int silnia(int n){
+	if(n <= 1){
+		return 1;
+	} else {
+		return n*silnia(n-1);
+	}
+}
+
 int main(){
 	int x;
 	std::cin >> x;
-	std::cout << x << std::endl;
+	std::cout << silnia(x) << std::endl;
 	return 0;
 }
