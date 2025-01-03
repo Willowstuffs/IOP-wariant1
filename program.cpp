1005220fab5ee54be6afedfce93c57f64065cbde
Wiktor Rembowski <wiktorrem@lts.mat.umk.pl>
2025-01-03 15:57:14 +0100

    Dodano do program.cpp sprawdzanie czy liczba podana przez uzytkownika jest nieujemna

--- program.cpp
+++ program.cpp
@@ -3,6 +3,11 @@
 int main(){
 	int x;
 	std::cin >> x;
+	if(x < 0){
+		std::cout << "Podano liczbe ujemna, program zakonczony." << std::endl;
+		return 1;
+	}
+
 	std::cout << x << std::endl;
 	return 0;
 }
